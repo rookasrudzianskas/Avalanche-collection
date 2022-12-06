6fc9cb864fe5f7fb1d6d987e2b944b12727b9886
Rokas <rookas.rudzenskas@gmail.com>
2022-12-06 18:50:24 +0200

    Add wallet address to global context
    This commit adds the wallet address to the global context.
    
    The wallet address is used to interact with smart contracts.

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -1,10 +1,14 @@
-import React, {createContext, useContext} from 'react';
+import React, {createContext, useContext, useState} from 'react';
 
 const GlobalContext = createContext();
 
 export const GlobalContextProvider = ({children}) => {
     // Interacting with smart contracts
+    const [walletAddress, setWalletAddress] = useState("");
 
+    const updateCurrentWalletAddress = async () => {
+        const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+    }
     return (
         <GlobalContext.Provider
             value={{
