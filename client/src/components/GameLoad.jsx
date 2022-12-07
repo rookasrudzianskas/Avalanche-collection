76787d1bec10df81e47fa51eeb5b4d56e16468fb
Rokas <rookas.rudzenskas@gmail.com>
2022-12-07 11:33:27 +0200

    Add player images to GameLoad component
    The images are used to show the player who is waiting for an opponent.
    
    The images are imported from the assets folder.

--- client/src/components/GameLoad.jsx
+++ client/src/components/GameLoad.jsx
@@ -3,6 +3,7 @@ import {useGlobalContext} from "../context/index.jsx";
 import {useNavigate} from "react-router-dom";
 import styles from "../styles";
 import {CustomButton} from "./index.js";
+import {player01, player02} from "../assets/index.js";
 
 const GameLoad = ({}) => {
     const { walletAddress } = useGlobalContext();
@@ -18,7 +19,28 @@ const GameLoad = ({}) => {
                 />
             </div>
 
+            <div className={`flex-1 ${styles.flexCenter} flex-col`}>
+                <h1 className={`${styles.headText} text-center`}>Waiting for a <br /> worthy opponent</h1>
+                <p className={styles.gameLoadText}>
+                    Pro-tip: while you're waiting, choose your preferred battleground
+                </p>
 
+                <div className={styles.gameLoadPlayersBox}>
+                    <div className={`${styles.flexCenter} flex-col`}>
+                        <img src={player01} className={styles.gameLoadPlayerImg} />
+                        <p className={styles.gameLoadPlayerText}>
+                            {walletAddress.slice(0, 9)}...{walletAddress.slice(-9)}
+                        </p>
+                    </div>
+
+                    <h2 className={styles.gameLoadVS}>Vs</h2>
+
+                    <div className={`${styles.flexCenter} flex-col`}>
+                        <img src={player02} className={styles.gameLoadPlayerImg} />
+                        <p className={styles.gameLoadPlayerText}>??????????</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
