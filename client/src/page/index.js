851c0166aadc67f319ad39cbb700d6d0a450034e
Rokas <rookas.rudzenskas@gmail.com>
2022-12-07 13:44:30 +0200

    Add battleground page
    This page will allow the user to choose a battleground for the battle.

--- client/src/page/index.js
+++ client/src/page/index.js
@@ -2,10 +2,12 @@ import Home from './Home';
 import CreateBattle from "./CreateBattle.jsx";
 import JoinBattle from "./JoinBattle.jsx";
 import Battle from "./Battle.jsx";
+import Battleground from "./Battleground.jsx";
 
 export {
     Home,
     CreateBattle,
     JoinBattle,
     Battle,
+    Battleground,
 }
